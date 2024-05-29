a95105dd2840d96756c5a120fcd9b31f7e7af0b8
Simone <simone@wimmweg.at>
2024-05-29 15:30:24 +0200

    add new components container and simpletext

---
+++ storyblok/Simpletext.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+      v-editable="blok"
+      class="py-2 text-start"
+  >
+    {{ blok.text }}
+  </div>
+</template>
+
+
+<script setup>
+defineProps({ blok: Object })
+</script>
