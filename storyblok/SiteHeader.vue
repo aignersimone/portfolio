3350fbd880ce46c629960efc8ef8dfc085104415
Simone <simone@wimmweg.at>
2024-06-07 18:05:25 +0200

    changing SiteHeader gap

--- storyblok/SiteHeader.vue
+++ storyblok/SiteHeader.vue
@@ -2,7 +2,7 @@
   <div class="py-4 mx-6 flex flex-wrap justify-center items-center md:justify-between my-3">
     <div class="text-3xl font-bold">Simone Aigner</div>
 
-    <div class="text-center md:text-right grid grid-cols-3 gap-4 items-center">
+    <div class="text-center md:text-right grid grid-cols-3 gap-4 md:gap-6 items-center">
       <MainNavigationItem
           v-for="item in $globalSettings.navbar"
           :key="item._uid"
