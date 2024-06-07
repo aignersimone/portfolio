17db712d66ce095c0acf2a0dfda628ada6953c03
Simone <simone@wimmweg.at>
2024-06-07 08:34:34 +0200

    changing Components and creating new components

--- storyblok/Header.vue
+++ storyblok/Header.vue
@@ -1,14 +1,14 @@
 <template>
-  <
+
     <div v-editable="blok" class="mb-5 bg-gray-300 pt-5 flex flex-wrap items-center justify-center">
 
-      <div class="lg:w-1/2 flex justify-center">
+      <div class="lg:w-1/2 flex justify-start lg:justify-center">
         <img :src="blok.image.filename" class="portraitimage" alt="Portrait"/>
       </div>
       <div class="lg:w-1/2 flex justify-start">
-        <div>
-          <h2 class="teaser text-2xl md:text-5xl align-baseline tracking-widest typed-text">{{blok.teaser}}</h2>
-          <p class="teaser  align-baseline pt-4 tracking-widest typed-text">{{blok.shortinfo}} </p>
+        <div class="">
+          <h2 class="teaser sm:text-2xl md:text-5xl align-baseline tracking-widest typed-text md:pl-0">{{blok.teaser}}</h2>
+          <p class="teaser text-base align-baseline pt-4 tracking-widest typed-text pl-0">{{blok.shortinfo}} </p>
         </div>
       </div>
     </div>
@@ -22,6 +22,17 @@ defineProps({ blok: Object });
 .teaser{
   color:White;
   margin-left: -200px;
+  word-wrap: break-word;
+}
+@media (max-width: 1050px) {
+  .teaser {
+    margin-left: 0; /* Padding entfernen für Bildschirme kleiner als lg */
+    margin-top: 10px;
+    text-align: center;
+    padding-top: 0;
+    padding-bottom: 10px;
+    word-wrap: break-word;
+  }
 }
 
 .portraitimage{
@@ -36,8 +47,6 @@ defineProps({ blok: Object });
     width: 100%;
   }
 }
-
-
 
 .typed-text {
   overflow: hidden;
--- storyblok/Zitat.vue
+++ storyblok/Zitat.vue
@@ -1,5 +1,5 @@
 <template>
-  <div  class="flex justify-center grid grid-rows-2 mx-6">
+  <div  class="flex justify-center grid grid-rows-2 mx-6 my-6">
     <div class="italic text-center text-lg">{{blok.zitat}}</div>
     <div class="text-center">{{ blok.author }}</div>
   </div>
