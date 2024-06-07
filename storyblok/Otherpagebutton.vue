5881be523282b7764af392d58b55eaf1fff4e375
Simone <simone@wimmweg.at>
2024-06-07 21:18:44 +0200

    changing components

--- storyblok/Otherpagebutton.vue
+++ storyblok/Otherpagebutton.vue
@@ -1,5 +1,7 @@
 <template>
+  <div class="pagebutton">
   <a :href="blok.link && blok.link.story ? blok.link.story.url: '#'"  class="mx-6 bg-blend-darken bg-gray-100 p-2 rounded-lg">{{blok.buttontext}}</a>
+  </div>
 </template>
 
 <script setup>
@@ -7,6 +9,9 @@ defineProps({ blok: Object });
 </script>
 
 <style scoped>
+.pagebutton{
+margin-top: -24px;
+}
 
 a:hover{
   background-color: #71792C;
