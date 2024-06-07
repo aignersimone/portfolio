3c0a3409cc6d15a1202ef7ea357c8ff70dc30448
Simone <simone@wimmweg.at>
2024-06-07 21:30:22 +0200

    changing components

--- storyblok/SiteHeader.vue
+++ storyblok/SiteHeader.vue
@@ -2,20 +2,21 @@
   <div class="py-4 mx-6 flex flex-wrap justify-center items-center md:justify-between my-3">
     <div class="text-3xl font-bold">Simone Aigner</div>
 
-    <div class="text-center md:text-right grid grid-cols-3 gap-4 md:gap-6 items-center">
+    <div class="text-center md:text-right flex gap-4 md:gap-6 items-center">
       <MainNavigationItem
           v-for="item in $globalSettings.navbar"
           :key="item._uid"
           :nav-item="item"
+          class="flex-shrink-0"
       ></MainNavigationItem>
     </div>
   </div>
 </template>
 
+
 <style scoped>
 
 </style>
-
 
 <script setup>
 defineProps({ blok: Object });
