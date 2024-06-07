7ab6f6cd44a83b8a8e5bc08ea819ea064ef1b364
Simone <simone@wimmweg.at>
2024-06-07 18:00:15 +0200

    changing Components and creating new components

--- storyblok/Zitat.vue
+++ storyblok/Zitat.vue
@@ -1,10 +1,19 @@
 <template>
-  <div  class="flex justify-center grid grid-rows-2 mx-6 my-6">
-    <div class="italic text-center text-lg">{{blok.zitat}}</div>
-    <div class="text-center">{{ blok.author }}</div>
+  <div class="flex justify-center mx-6 my-6">
+    <div class="grid grid-rows-auto">
+      <div class="italic text-center text-lg">{{ blok.zitat }}</div>
+      <div class="text-center">{{ blok.author }}</div>
+    </div>
   </div>
 </template>
 
 <script setup>
 defineProps({ blok: Object })
 </script>
+
+<style scoped>
+.grid-rows-auto {
+  display: grid;
+  grid-template-rows: auto auto;
+}
+</style>
