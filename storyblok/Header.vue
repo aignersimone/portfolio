849bde62418f5f539fe62509e118b295c4350e45
Simone <simone@wimmweg.at>
2024-06-10 13:34:27 +0200

    added logo and shadow to btn

--- storyblok/Header.vue
+++ storyblok/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-editable="blok" class="mb-5 bg-gray-300 pt-5 flex flex-wrap items-center justify-center">
-      <div class="lg:w-1/2 flex justify-start lg:justify-center">
+      <div class=" imagediv lg:w-1/2 flex justify-start lg:justify-center">
         <img :src="blok.image.filename" class="portraitimage" alt="Portrait"/>
       </div>
       <div class="lg:w-1/2 flex justify-start">
@@ -27,17 +27,26 @@ defineProps<{ blok: Header }>();
 <style scoped>
 .teaser{
   color:White;
-  margin-left: -200px;
+  /*margin-left: -200px;*/
   word-wrap: break-word;
 }
+
+.imagediv{
+  margin-right: -200px;
+}
+
 @media (max-width: 1050px) {
   .teaser {
-    margin-left: 0; /* Padding entfernen für Bildschirme kleiner als lg */
+    margin-left: 0;
     margin-top: 10px;
     text-align: center;
     padding-top: 0;
     padding-bottom: 10px;
     word-wrap: break-word;
+  }
+
+  .imagediv{
+    margin-right: 0;
   }
 }
 
@@ -57,7 +66,7 @@ defineProps<{ blok: Header }>();
 .typed-text {
   overflow: hidden;
   white-space: nowrap;
-  animation: typing 3s steps(40, end); /* Geschwindigkeit der Animation und Anzahl der Schritte (Buchstaben) */
+  animation: typing 3s steps(40, end);
 }
 
 .typed-teaser {
