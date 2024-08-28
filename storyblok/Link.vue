e3d6c6bf303ed6be8f36d45ca9bfbbb8e5a384ac
Simone <simone@wimmweg.at>
2024-08-28 09:07:26 +0200

    changed readme, deleted container, changed life+project container

---
+++ storyblok/Link.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+      v-editable="blok"
+      class="blocktext text-lg text-start mx-6 mb-6"  >
+    {{ blok.teaser }}
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import type {SbComponent} from "../types/sb-component";
+
+interface Link extends SbComponent{
+  teaser: string;
+}
+
+defineProps<{ blok: Link }>();
+</script>
+
+<style scoped>
+.blocktext{
+  text-align: justify;
+}
+</style>
+
+
+
